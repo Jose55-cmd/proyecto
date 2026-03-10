243c39ca2fd02d2662a0cc40c996e2c184904856
Jose55-cmd <joseyanesv87@gmail.com>
2026-03-10 12:24:41 -0400

    Delete URL.py

--- URL.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Cuando el usuario entre a midominio.com/dashboard/
-    path('dashboard/', views.dashboard_view, name='dashboard'), # type: ignore
-]
